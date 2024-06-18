9cf1ba118a0320be9ec161cc3db5f40790138125
NIKHIL <nikhilnerkar8@gmail.com>
2024-06-18 21:37:28 +0530

    py

---
+++ LearningPyhton/1_Operetors.py
@@ -0,0 +1,57 @@
+#arithmetics operetors
+x = 10
+y= 5
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+#assignments operators
+x = 10
+x=x+2
+print(x)
+
+x = 10
+x=x-2
+print(x)
+
+x = 10
+x=x*2
+print(x)
+
+x = 10
+x=x/2
+print(x)
+
+x = 10
+x=x**2
+print(x)
+
+x = 10
+x=x//2
+print(x)
+
+
+#comparison operetors
+
+x = 10
+y = 5
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x <= y)
+print(x >= y)
+
+#logical operetors
+x = 10
+y = 5
+
+print(x == y and x > y)
+print(x != y or x < y)
+
